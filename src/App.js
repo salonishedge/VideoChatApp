11215dafcf290e56b22d5b0d55dd8ffe0b13a667
Saloni Shedge <salonishedge09@gmail.com>
2021-07-08 19:55:37 +0530

    Creation of frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import {Typography,AppBar} from '@material-ui/core';
+import VideoPlayer  from './components/VideoPlayer';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyle = makeStyles((theme)=> ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+
+}));
 
 function App() {
+  const classes= useStyle();
   return (
-    <div className="App">
-      <AppBar position="static" color="inherit">
+    <div className={classes.wrapper}>
+      <AppBar position="static" color="inherit" className={classes.appBar}>
         <Typography variant="h2" align="center">Video Chat</Typography>
       </AppBar>
+      <VideoPlayer/>
+      <Options>
+        <Notifications/>
+      </Options>
       {/*Video Player */}
       {/*Options > Notifications */}
     </div>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ContextProvider } from './SocketContext';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ContextProvider>
+      <App />
+    </ContextProvider>,
   document.getElementById('root')
 );
 
